f6a7fd0b98613747a7499ff70d2b941287fb17ea
U1ltra <jiaruili0000@gmail.com>
2026-04-11 00:00:50 -0400

    chord: time buffer

--- utils/exp_utils.py
+++ utils/exp_utils.py
@@ -653,7 +653,7 @@ def launch_scripts(args, trial_configs, trial_index):
 
     if args.realtime_factor == 1.0 or args.realtime_factor == 0.5:
         if args.world_file in ["field", "urban", "urban_car"]:
-            time.sleep(50)
+            time.sleep(55)
         else:
             time.sleep(25)
     else:
